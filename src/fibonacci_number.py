116e40f2edfea19ff5f7485b0f4bc54a4f06c787
Gustavo Saiani <gustavo@poe.ma>
2026-05-26 16:12:58 -0300

    Fibonacci Number (#509) (#1)
    * Fibonacci Number (#509)
    
    * Fibonacci Number (#509)

--- src/fibonacci_number.py
+++ src/fibonacci_number.py
@@ -14,6 +14,7 @@ Given n, calculate F(n).
 
 
 def fib(n: int) -> int:
-    """Return the n-th Fibonacci number."""
-    _ = n
-    raise NotImplementedError
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
